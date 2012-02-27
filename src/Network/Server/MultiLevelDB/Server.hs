95b60e7c5a35d55e3d47d5fe75daae02cb5a9a3a
Will Moss <wmoss@bu.mp>
2012-02-27 08:25:07 -0800

    No reason to be calling withLevelDB every time, let's just pass it in

--- src/Network/Server/MultiLevelDB/Server.hs
+++ src/Network/Server/MultiLevelDB/Server.hs
@@ -29,25 +29,24 @@ parseRequest = do
     raw <- Atto.take $ fromIntegral size
     return $ Request rid $ B.fromChunks [raw]
 
-handleRequest :: Request -> IO Builder
+handleRequest :: DB -> Request -> IO Builder
 
-handleRequest (Request MULTI_LEVELDB_GET raw) = do
-    withLevelDB "/tmp/leveltest" [ CreateIfMissing, CacheSize 2048 ] $ \db -> do
-        res <- get db [ ] $ lTos $ Get.key obj
-        case res of
-            Just v -> return $ copyByteString $ S.concat [v, "\r\n"]
-            Nothing -> return $ copyByteString "MISSING\r\n"
+handleRequest db (Request MULTI_LEVELDB_GET raw) = do
+    res <- get db [ ] $ lTos $ Get.key obj
+    case res of
+        Just v -> return $ copyByteString $ S.concat [v, "\r\n"]
+        Nothing -> return $ copyByteString "MISSING\r\n"
     where
         obj = decodeProto raw :: Get.GetRequest
 
-handleRequest (Request MULTI_LEVELDB_PUT raw) = do
-    withLevelDB "/tmp/leveltest" [ CreateIfMissing, CacheSize 2048 ] $ \db -> do
-        put db [ ] (lTos $ Put.key obj) (lTos $ Put.value obj)
-        return $ copyByteString "OK\r\n"
+handleRequest db (Request MULTI_LEVELDB_PUT raw) = do
+    put db [ ] (lTos $ Put.key obj) (lTos $ Put.value obj)
+    return $ copyByteString "OK\r\n"
     where
         obj = decodeProto raw :: Put.PutRequest
 
 main = do
-    runServer pipe 4455
+    withLevelDB "/tmp/leveltest" [ CreateIfMissing, CacheSize 2048 ] $ \db -> do
+        runServer (pipe db) 4455
     where
-        pipe = RequestPipeline parseRequest handleRequest 10
+        pipe db = RequestPipeline parseRequest (handleRequest db) 10
